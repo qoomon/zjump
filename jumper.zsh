0916d626f371416ef55fced75a53efdcf26c80f1
Bengt Brodersen <bengt.brodersen@gmail.com>
2017-05-28 18:19:03 +0200

    refactor

--- jumper.zsh
+++ jumper.zsh
@@ -7,62 +7,62 @@ function j {
     echo "couldn't find fzf installation" >&2
     return 1
   fi
-
+  
   local cmd="$1"
   case "$cmd" in
-    '..')
+    '...') # parent folder selection
       shift;
       local pwd_list=('/' ${(s:/:)PWD%/*})
       local indexed_pwd_list=()
-      for pwd_part_index in $(seq 1 ${#pwd_list}); do
+      for pwd_part_index in {1..${#pwd_list}}; do
           indexed_pwd_list[$pwd_part_index]="$pwd_part_index $pwd_list[$pwd_part_index]"
       done
-      local pwd_index="$(printf "%s\n" "${indexed_pwd_list[@]}" | fzf --tac --height 10 --reverse --prompt='  ' --exact --with-nth=2.. | awk '{print $1}')"
-      if [ -n "$pwd_index" ]; then
-        local dir_list=(${pwd_list:0:$pwd_index})
-        local dir="${(j:/:)dir_list}"
-        builtin cd "$dir"
-      else
+      local pwd_index
+      pwd_index="$(echo "${(F)indexed_pwd_list}" | fzf --tac --height 10 --reverse --prompt='  ' --exact --with-nth=2.. | awk '{print $1}')"
+      if [[ $status == 1 ]]; then
         echo "no directory matches" >&2
         return 1
+      elif [[ $status == 130 ]]; then
+        return 0
       fi
+      
+      local dir_list=(${pwd_list:0:$pwd_index})
+      local dir="${(j:/:)dir_list}"
+      builtin cd $dir
       ;;
-    '.')
+    '.') # subfolder selection
       shift;
       local dir_query="$*"
-      local dir=$(find . -mindepth 1 \( -type f -o -path '*/.*/*' \) -prune -o -print 2>&1 | sed 's|^\./\(.*\)|\1|' | fzf --tac --height 10 --reverse --prompt='  ' --query "$dir_query" --exact --select-1 --exit-0)
-      if [ -n "$dir" ]; then
-        builtin cd "$dir"
-      else
+      local dir
+      dir=$(find . -mindepth 1 -type d 2>&1 | grep -v 'find:.*Permission denied' | sed 's|^\./\(.*\)|\1|' | fzf --tac --height 10 --reverse --prompt='  ' --query "$dir_query" --exact --select-1 --exit-0)
+      if [[ $status == 1 ]]; then
         echo "no directory matches" >&2
         return 1
+      elif [[ $status == 130 ]]; then
+        return 0
       fi
+      
+      builtin cd $dir
+      ;;
+    ':') # historyfolder selection
+      shift;
+      local dir_query="$@"
+      local dir
+      dir="$(cdr -l | while read -r line; do echo "${${=line}[@]:1}"; done | fzf  --height 10 --reverse  --prompt='  ' --query "$dir_query" --exact --select-1 --exit-0)"
+      if [[ $status == 1 ]]; then
+        echo "no directory matches" >&2
+        return 1
+      elif [[ $status == 130 ]]; then
+        return 0
+      fi
+      
+      dir=${dir/#'~'/$HOME}
+      builtin cd $dir
       ;;
     *)
-      local dir_query="$@"
-      local dir="$(cdr -l | awk '{$1=""; print $0}' | fzf  --height 10 --reverse  --prompt='  ' --query "$dir_query" --exact --select-1 --exit-0)"
-      if [ -n "$dir" ]; then
-        eval "builtin cd ${dir}"
-      else
-        echo "no directory matches" >&2
-        return 1
-      fi
-      ;;
+      builtin cd $@ >/dev/null # suppress stdout
+      ;;  
   esac
-}
-
-#enhanced cd
-function j::cd {
-  if [ "$1" = ':' ]; then #  historyfolder selection
-    shift
-    j $@
-  elif [ "$1" = '...' ]; then # parent folder selection
-    shift
-    j .. $@
-  elif [ "$1" = '.' ]; then # subfolder selection
-    shift
-    j . $@
-  else
-    builtin cd $@ >/dev/null # suppress stdout
-  fi
+  
+  
 }
