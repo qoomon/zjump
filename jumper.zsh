08766664780f0fe3684cf65a19b9c9195bff764b
Bengt Brodersen <bengt.brodersen@gmail.com>
2017-05-30 13:43:32 +0200

    refactor: cleanup

--- jumper.zsh
+++ jumper.zsh
@@ -13,27 +13,31 @@ function j {
   case "$cmd" in
     '...') # parent folder selection
       shift;
-      local pwd_list=('/' ${(s:/:)PWD%/*})
+      local dir_query=$@
+      
+      local pwd_list=('/' '/'${^${(s:/:)PWD%/*}})
       local indexed_pwd_list=()
       for pwd_part_index in {1..${#pwd_list}}; do
           indexed_pwd_list[$pwd_part_index]="$pwd_part_index $pwd_list[$pwd_part_index]"
       done
+      
       local pwd_index
-      pwd_index="$(echo "${(F)indexed_pwd_list}" | fzf --tac --height 10 --reverse --prompt='  ' --exact --with-nth=2.. | awk '{print $1}')"
+      pwd_index=$(echo ${(F)indexed_pwd_list} | fzf --tac --height 10 --reverse --prompt='  ' --query "$dir_query" --exact --select-1 --exit-0 --with-nth=2..) \
+        && pwd_index=${${=pwd_index}[@]:0:1}
       if [[ $status == 1 ]]; then
         echo "no directory matches" >&2
         return 1
       elif [[ $status == 130 ]]; then
         return 0
       fi
-      
-      local dir_list=(${pwd_list:0:$pwd_index})
-      local dir="${(j:/:)dir_list}"
+
+      local dir=${${(j::)pwd_list:0:$pwd_index}//#\/\//\/} # remove double
       builtin cd $dir
       ;;
     '.') # subfolder selection
       shift;
-      local dir_query="$*"
+      local dir_query=$@
+      
       local dir
       dir=$(find . -mindepth 1 -type d 2>&1 | grep -v 'find:.*Permission denied' | sed 's|^\./\(.*\)|\1|' | fzf --tac --height 10 --reverse --prompt='  ' --query "$dir_query" --exact --select-1 --exit-0)
       if [[ $status == 1 ]]; then
@@ -47,9 +51,11 @@ function j {
       ;;
     ':') # historyfolder selection
       shift;
-      local dir_query="$@"
+      local dir_query=$@
+      
       local dir
-      dir="$(cdr -l | while read -r line; do echo "${${=line}[@]:1}"; done | fzf  --height 10 --reverse  --prompt='  ' --query "$dir_query" --exact --select-1 --exit-0)"
+      dir=$((for entry (${(f)"$(cdr -l)"}) echo ${${=entry}[@]:1}) | fzf  --height 10 --reverse  --prompt='  ' --query "$dir_query" --exact --select-1 --exit-0) \
+        && dir=${dir/#'~'/$HOME}
       if [[ $status == 1 ]]; then
         echo "no directory matches" >&2
         return 1
@@ -57,7 +63,6 @@ function j {
         return 0
       fi
       
-      dir=${dir/#'~'/$HOME}
       builtin cd $dir
       ;;
     *)
