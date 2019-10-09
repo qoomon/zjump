18f5665d25dd83fe458ed4f37ff052d140398ba4
Bengt Brodersen <bengt.brodersen@otto.de>
2019-10-09 15:36:42 +0200

    refactor: streamline output of parent directories

--- zjump.zsh
+++ zjump.zsh
@@ -39,57 +39,51 @@ function zjump {
       
       shift;
       ;;
+      
     '..') # parent directories selection
       shift;
       local dir_query=$@
       
-      local pwd_list=('/' '/'${^${(s:/:)PWD%/*}})
-      local pwd_index
-      pwd_index=$(echo ${(F)pwd_list} | nl \
-          | fzf --tac --height 10 --reverse --no-sort --query "$dir_query" --exact --select-1 --with-nth=2.. \
-          | cut -f1)
-      return_code=$status
-      if [[ $return_code == 1 ]]; then
-        echo "no match" >&2
-        return 1
-      elif [[ $return_code == 130 ]]; then
-        return 0
+      local dir_list=(${(s:/:)PWD%/*})
+      dir_list=$(for dir_index in {0..$((${#dir_list}))}; echo /${(j:/:)dir_list:0:$dir_index})
+      local dir # local declaration needs a seperate line to be able to catch fzf_status
+      dir=$(echo ${dir_list}\
+          | fzf --tac --height 10 --reverse --no-sort --query "$dir_query" --exact --select-1)
+      local fzf_status=$status
+      if [[ $fzf_status != 0 ]]; then
+        return $fzf_status
       fi
-
-      local dir=${(j::)pwd_list:0:$pwd_index} # remove double
+      
       builtin cd $dir
       ;;
+      
     '.') # sub-directories selection
       shift;
       local dir_query=$@
       
-      local dir
+      local dir # local declaration needs a seperate line to be able to catch fzf_status
       dir=$(find . -mindepth 1 -type d 2>&1 \
           | grep -v 'find:.*Permission denied' \
           | sed 's|^\./\(.*\)|\1|' \
           | fzf --tac --height 10 --reverse --query "$dir_query" --exact --select-1)
-      return_code=$status
-      if [[ $return_code == 1 ]]; then
-        echo "no match" >&2
-        return 1
-      elif [[ $return_code == 130 ]]; then
-        return 0
+      local fzf_status=$status
+      if [[ $fzf_status != 0 ]]; then
+        return $fzf_status
       fi
       
       builtin cd $dir
       ;;
+      
     *) # history directories selection
       local dir_query=$@
       
-      local dir
-      dir=$( cdr -l | sed 's|^[^ ]* *||' | sed 's|\\\(.\)|\1|g' | sed "s|^~|$HOME|" \
+      local dir # local declaration needs a seperate line to be able to catch fzf_status
+      dir=$( cdr -l | sed 's|^[^ ]* *||' | sed 's|\\\(.\)|\1|g' \
+          | sed "s|^~|$HOME|" \
           | fzf --height 10 --reverse --query "$dir_query" --exact --select-1)
-      return_code=$status
-      if [[ $return_code == 1 ]]; then
-        echo "no match" >&2
-        return 1
-      elif [[ $return_code == 130 ]]; then
-        return 0
+      local fzf_status=$status
+      if [[ $fzf_status != 0 ]]; then
+        return $return_code
       fi
       
       builtin cd $dir
